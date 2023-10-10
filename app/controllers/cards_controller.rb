8c9f9bc1656245a0c69c3eb95eaaaba3e7657ab1
claudiaamprimo <claudia.amprimo@gmail.com>
2023-10-10 13:27:28 +0200

    added pokemon api

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,7 @@
 class CardsController < ApplicationController
   def index
     @cards = policy_scope(Card)
+    # @cards = Card.all
   end
 
   def show
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,5 @@
 class Collection < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :cards
+  # has_and_belongs_to_many :cards
+  has_many :cards
 end
