a34c0413b6f29d00f2172bbac54db83d8148c7b9
claudiaamprimo <claudia.amprimo@gmail.com>
2023-10-30 15:51:53 +0100

    added cloudinary to avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit::Authorization
 
@@ -15,7 +16,12 @@ class ApplicationController < ActionController::Base
   # end
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :photo])
+  end
+
   private
 
   def skip_pundit?
